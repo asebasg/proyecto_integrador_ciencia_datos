4a75c6a43a99edb703d034795db75694ae730cb2
av164702gmail.com <av164702gmail.com>
2025-11-30 00:25:04 -0500

    Integracion del preprocessing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,21 @@
 # Asignado a Ricardo (@ricardo778)
+from .data_loader import cargar_datos, verificar_duplicados
+from .preprocessing import (
+    calcular_tasas, 
+    filtrar_por_municipio, 
+    agrupar_por_año, 
+    agrupar_por_municipio,
+    crear_categorias_riesgo,
+    limpiar_datos_faltantes
+)
+
+__all__ = [
+    'cargar_datos',
+    'verificar_duplicados',
+    'calcular_tasas',
+    'filtrar_por_municipio', 
+    'agrupar_por_año',
+    'agrupar_por_municipio',
+    'crear_categorias_riesgo',
+    'limpiar_datos_faltantes'
+]
